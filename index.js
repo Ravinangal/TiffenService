a13dbbb5f6db04417eb67dc1f6d284e8d8ec859a
ravi samra <ravinangal3@gmail.com>
2023-08-23 13:31:14 +0530

    Breakfast page is built with cart ....

--- index.js
+++ index.js
@@ -11,22 +11,49 @@ $(document).ready(function(){
 });
 
 document.addEventListener('DOMContentLoaded', function () {
-    const plusBtn = document.querySelector('.plus-btn');
-    const minusBtn = document.querySelector('.minus-btn');
-    const productCount = document.querySelector('.product-count');
-  
+  const products = document.querySelectorAll('.product');
+  const cartItemsTable = document.querySelector('.cart-items tbody');
+
+  products.forEach(product => {
+    const plusBtn = product.querySelector('.plus-btn');
+    const minusBtn = product.querySelector('.minus-btn');
+    const productCount = product.querySelector('.product-count');
+
     let count = 0;
-  
+
     plusBtn.addEventListener('click', function () {
       count++;
       productCount.textContent = count;
+      updateCart(product, count);
     });
-  
+
     minusBtn.addEventListener('click', function () {
       if (count > 0) {
         count--;
         productCount.textContent = count;
+        updateCart(product, count);
       }
     });
   });
-    
+
+  function updateCart(product, count) {
+    const productName = product.querySelector('img').alt;
+    const existingCartItem = cartItemsTable.querySelector(`tr[data-product="${productName}"]`);
+
+    if (count === 0 && existingCartItem) {
+      existingCartItem.remove();
+    } else if (count > 0) {
+      if (existingCartItem) {
+        existingCartItem.querySelector('.cart-item-count').textContent = count;
+      } else {
+        const cartRow = document.createElement('tr');
+        cartRow.dataset.product = productName;
+        cartRow.innerHTML = `
+          <td>${productName}</td>
+          <td class="cart-item-count">${count}</td>
+        `;
+        cartItemsTable.appendChild(cartRow);
+      }
+    }
+  }
+});
